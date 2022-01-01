10302f5b8bac983c7ea11f1695ea0aa9e5d2f4f2
Bruno Braga <brunobragaw8t@gmail.com>
2022-01-01 22:21:59 +0000

    Create footer

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+export const Footer = () => {
+  return (
+    <footer className="py-4 bg-dark text-light">
+      <div className="container text-center">
+        <small>
+          2021 &copy; Movies DB. Developed by <a href="https://github.com/brunobragaw8t" target="_blank">Bruno Braga</a>
+        </small>
+      </div>
+    </footer>
+  )
+}
